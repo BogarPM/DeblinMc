28064334b5e3fb2f80089ef355309d8241d59670
DRK970 <bogarperezmireles@gmail.com>
2020-12-19 16:17:20 -0600

    added the main class

---
+++ src/src/DeblinMc.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Bogar
+ */
+public class DeblinMc extends JavaPlugin{
+
+    @Override
+    public void onEnable() {
+        Bukkit.broadcastMessage("Server enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
